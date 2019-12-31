6b4f52a22459582edff515e3d66469ccba6e7bca
Xie ZhongYi <837447352@qq.com>
2019-12-31 12:25:53 +0800

    忘记密码模块 (手机验证码以及验证）

--- src/components/ForgetPassword.vue
+++ src/components/ForgetPassword.vue
@@ -5,42 +5,135 @@
         用手机号重置密码
       </h4>
       <div class="sign_in">
-        <el-input
-          class="telephone"
-          placeholder="手机号"
-          prefix-icon="el-icon-mobile-phone" v-model="telephone">
-        </el-input>
-        <el-input
-          class="code"
-          placeholder="短信验证码"
-          prefix-icon="el-icon-message" v-model="code">
-          <el-button class="sendCode" slot="suffix" size="mini" round>发送验证码</el-button>
-        </el-input>
-        <el-input
-          class="password"
-          placeholder="请输入新密码"
-          prefix-icon="el-icon-lock" show-password v-model="password">
-        </el-input>
-        <el-input
-          class="password"
-          placeholder="请再一次输入新密码"
-          prefix-icon="el-icon-lock" show-password v-model="repassword">
-        </el-input>
+        <el-form :model="userForm" :rules="rules"  ref="userForm">
+          <el-form-item prop="telephone">
+            <el-input
+              class="telephone"
+              placeholder="手机号"
+              prefix-icon="el-icon-mobile-phone" v-model.number="userForm.telephone">
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              class="code"
+              placeholder="短信验证码"
+              prefix-icon="el-icon-message" v-model="userForm.code">
+              <el-button @click="sendMsg" :disabled="isDisabled" class="sendCode" slot="suffix" size="mini" round>{{buttonName}}</el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              class="password"
+              placeholder="请输入新密码"
+              prefix-icon="el-icon-lock" show-password v-model="userForm.password">
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="repassword">
+            <el-input
+              class="password"
+              placeholder="请再一次输入新密码"
+              prefix-icon="el-icon-lock" show-password v-model="userForm.repassword">
+            </el-input>
+          </el-form-item>
+        </el-form>
       </div>
-      <el-button class="reset_button" round>重置密码</el-button>
+      <el-button class="reset_button" round @click="submitForm('userForm')">重置密码</el-button>
       <el-link class="return" :underline="false" href="#/sign_in"><i class="el-icon-back"/>返回登录注册</el-link>
     </div>
   </div>
 </template>
 <script>
+const TIME_COUNT = 10 // 倒计时的时间
 export default {
   name: 'ForgetPassword',
   data () {
+    const CheckTel = (rule, value, callback) => {
+      this.isDisabled = true
+      if (!value) {
+        callback(new Error('请输入手机号码'))
+      } else if (!Number.isInteger(value)) {
+        callback(new Error('手机号码必须是数字'))
+      } else if (value.toString().length !== 11) {
+        callback(new Error('手机号码必须是11位'))
+      } else {
+        callback()
+        this.isDisabled = false
+      }
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else if (value.toString().length < 8 || value.toString().length > 16) {
+        callback(new Error('密码长度为8-16位'))
+      } else {
+        if (this.userForm.password !== '') {
+          this.$refs.userForm.validateField('repassword')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.userForm.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
-      telephone: '',
-      code: '',
-      password: '',
-      repassword: ''
+      userForm: {
+        telephone: '',
+        code: '',
+        password: '',
+        repassword: ''
+      },
+      rules: {
+        telephone: [
+          { required: true, validator: CheckTel, trigger: ['blur', 'change'] }
+        ],
+        password: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        repassword: [
+          { validator: validatePass2, trigger: 'blur' }
+        ]
+      },
+      isDisabled: true,
+      buttonName: '发送验证码',
+      timer: null
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+          console.log(this.userForm)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    sendMsg () {
+      // 验证码倒计时
+      if (!this.timer) {
+        this.count = TIME_COUNT
+        this.isDisabled = false
+        this.timer = setInterval(() => {
+          if (this.count > 0 && this.count <= TIME_COUNT) {
+            this.count--
+            this.buttonName = '重新发送（' + this.count + '秒' + '）'
+            this.isDisabled = true
+          } else {
+            this.isDisabled = false
+            clearInterval(this.timer)
+            this.timer = null
+            this.buttonName = '重新发送'
+          }
+        }, 1000)
+      }
     }
   }
 }
@@ -86,15 +179,6 @@ export default {
     font-weight: 700;
     color: #ea6f5a;
     border-bottom: 3px solid #ea6f5a;
-  }
-  .telephone {
-    height: 50px;
-  }
-  .code {
-    height: 50px;
-  }
-  .password {
-    height: 50px;
   }
   .reset_button {
     margin-top: 20px;
